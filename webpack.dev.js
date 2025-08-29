2ffc4905cc1eed22915267bf04732c5647a6ec42
Ekio <141391736+Eki213@users.noreply.github.com>
2025-08-29 07:51:24 -0300

    Initial commit

---
+++ webpack.dev.js
@@ -0,0 +1,20 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "eval-source-map",
+  devServer: {
+    static: "./dist",
+    watchFiles: ["./src/template.html"],
+    port: 5600,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+});
